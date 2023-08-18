15e176afc509733526aac81c09370945d0845fbc
EunseokKang <es060604@gmail.com>
2023-08-18 20:50:45 +0900

    DB

---
+++ myWebProject/server.js
@@ -0,0 +1,32 @@
+// app.js 또는 server.js 등의 이름으로 메인 서버 파일을 작성
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+require("dotenv").config(); // 환경 변수 설정용
+
+// 애플리케이션 객체 인스턴스화 및 포트 설정 
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// 환경 변수에 저장된 PORT 값 불러오기 (기본값: 3000)
+const PORT = process.env.PORT || 3000;
+
+// DB 연결 문자열 구성 및 연결 성공, 실패 처리
+const DB_URI = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB 연결 성공'))
+  .catch((err) => console.log('MongoDB 연결 실패:', err));
+
+// 기본 GET 요청 처리
+app.get('/', (req, res) => {
+    res.send('환영합니다! 이곳은 Node.js와 MongoDB를 사용한 웹입니다.');
+});
+
+// 서버 실행
+app.listen(PORT, () => {
+    console.log(`서버가 http://localhost:${PORT}에서 정상 작동 중입니다.`);
+});
